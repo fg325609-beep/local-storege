3181fc8985de8782fa08e0ba32f5a29dc5c835be
Isming <email@example.com>
2026-04-06 19:48:35 +0500

    mmmmm

--- main.js
+++ main.js
@@ -2,3 +2,19 @@
 //  let [a,b,c,d,e,] = meva;
 // console.log("distiraction assigin usuli",meva);
 
+
+
+
+// let odam = {
+//     id :1 ,
+//     name : "fara" ,
+//     age : 20 ,
+//     oqish: "IT",
+
+// }
+// let {id,name,age,oqish} = odam;
+// console.log(id, name, age ,oqish);
+
+
+
+
