85d362754f141c9eca7fb2713f8e7de1ad37cb66
Isming <email@example.com>
2026-04-07 07:23:51 +0500

    hjhjjhj

--- main.js
+++ main.js
@@ -18,3 +18,53 @@
 
 
 
+let odamlar = [
+  {
+    "id": 1,
+    "ism": "Farhod",
+    "yosh": 28,
+    "shahar": "Toshkent",
+    "kasb": "Dasturchi",
+    "email": "farhod@example.com"
+  },
+  {
+    "id": 2,
+    "ism": "Dilnoza",
+    "yosh": 24,
+    "shahar": "Samarqand",
+    "kasb": "Dizayner",
+    "email": "dilnoza@example.com"
+  },
+  {
+    "id": 3,
+    "ism": "Shohruh",
+    "yosh": 31,
+    "shahar": "Buxoro",
+    "kasb": "Muhandis",
+    "email": "shohruh@example.com"
+  },
+  {
+    "id": 4,
+    "ism": "Madina",
+    "yosh": 22,
+    "shahar": "Andijon",
+    "kasb": "O'qituvchi",
+    "email": "madina@example.com"
+  },
+  {
+    "id": 5,
+    "ism": "Rustam",
+    "yosh": 35,
+    "shahar": "Farg'ona",
+    "kasb": "Savdo menejeri",
+    "email": "rustam@example.com"
+  },
+  {
+    "id": 6,
+    "ism": "Zuhra",
+    "yosh": 26,
+    "shahar": "Namangan",
+    "kasb": "Advokat",
+    "email": "zuhra@example.com"
+  }
+];
